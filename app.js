d1525629781dff4ce198f27378043756bd380617
Alexey Kremnev <alkremn@gmail.com>
2021-08-18 15:14:12 -0600

    feat: connect to database and implement model classes

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+
 const app = express();
+const mongoose = require('mongoose');
+
+// database connection
+mongoose.connect('mongodb://localhost/bitfilmsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
 
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = model('User', userSchema);
